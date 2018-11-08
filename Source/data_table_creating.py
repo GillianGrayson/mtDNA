b7544eb6ecf2f4b0e91218106ef4ff3ec75cdc6f
Unknown <aaron.blare@mail.ru>
2018-11-08 18:23:54 +0300

    data table creating

---
+++ Source/data_table_creating.py
@@ -0,0 +1,20 @@
+import os
+
+data_path = '../Data/'
+data_table_file_name = 'data_snp.txt'
+data_file = open(data_path + data_table_file_name, 'w')
+
+header = ''
+for dir_name in os.listdir(data_path):
+    if dir_name.startswith('chr'):
+        chr_path = data_path + dir_name + '/'
+        for gene_file_name in os.listdir(chr_path):
+            f = open(chr_path + gene_file_name)
+            for line in f:
+                if header == '':
+                    header = line
+                    data_file.write(header)
+                else:
+                    data_file.write(line)
+            f.close()
+data_file.close()
--- Source/genes_snps_counting.py
+++ Source/genes_snps_counting.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 data_path = '../Data/'
-target_chromosome = 'chr11'
+target_chromosome = 'chr7'
 result_path = data_path + target_chromosome + '/'
 count_path = data_path + '/snp_counts/'
 if not os.path.exists(count_path):
