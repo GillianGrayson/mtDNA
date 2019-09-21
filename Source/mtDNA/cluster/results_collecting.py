98d46e234b33a2c2ada246061c151b88366147d6
Unknown <aaron.blare@mail.ru>
2019-09-21 16:28:30 +0300

    Refactoring

--- Source/mtDNA/cluster/results_collecting.py
+++ Source/mtDNA/cluster/results_collecting.py
@@ -1,4 +1,5 @@
 import os
+from Source.mtDNA.cluster.rf import random_forest
 
 data_path = '../../../Data/'
 experiment_type = 'nuc'
@@ -7,10 +8,8 @@ target_pop = 'FIN'
 reference_part = 0.75
 result_file_suffix = 'cold'
 target_accuracy = 0.68
-num_features = 0
+num_features = 50
 gene_files = ['test_gene_list_cold_adaptation.txt']
-k_mt_max = 1
-k_nuc_max = 2
 top_results_num = 5
 
 result_path = '../../../Result/files/'
@@ -18,15 +17,127 @@ experiment_result_path = result_path + experiment_type + '/' + \
                          'ref(' + reference_pop + ')_' + 'target(' + target_pop + ')/'
 
 top_accuracy = []
-top_genes = []
+top_indices = []
+top_dirs = []
+top_genes_mt = []
+top_genes_nuc = []
 
 min_accuracy_value = 0.0
 min_accuracy_index = top_results_num
 
-for (dirpath, dirnames, filenames) in os.walk(experiment_result_path):
-    for filename in filenames:
-        if filename.startswith('accuracy'):
-            f = open(filename)
+dirnames = [x[0] for x in os.walk(experiment_result_path)][1:]
+
+for dirname in dirnames:
+    for filename in os.listdir(dirname):
+        if 'accuracy' in filename:
+            f = open(dirname + '/' + filename)
+            line_num = 0
             for line in f:
                 item = line.replace('\n', '')
-                if float(item) > min_accuracy_value:
+                if len(top_accuracy) < top_results_num:
+                    top_accuracy.append(float(item))
+                    min_accuracy_value = min(top_accuracy)
+                    min_accuracy_index = top_accuracy.index(min_accuracy_value)
+                    top_indices.append(line_num)
+                    top_dirs.append(dirname)
+                else:
+                    if float(item) > min_accuracy_value:
+                        top_accuracy[min_accuracy_index] = float(item)
+                        min_accuracy_value = min(top_accuracy)
+                        min_accuracy_index = top_accuracy.index(min_accuracy_value)
+                        top_indices[min_accuracy_index] = line_num
+                        top_dirs[min_accuracy_index] = dirname
+                line_num += 1
+            f.close()
+
+if experiment_type == 'mt':
+    for id in range(0, len(top_dirs)):
+        for file in os.listdir(top_dirs[id]):
+            if 'genes_mt' in file:
+                f = open(top_dirs[id] + '/' + file)
+                for i, line in enumerate(f):
+                    if i == top_indices[id]:
+                        top_genes_mt.append(line.replace('\n', '').split('\t'))
+                f.close()
+elif experiment_type == 'nuc':
+    for id in range(0, len(top_dirs)):
+        for file in os.listdir(top_dirs[id]):
+            if 'genes_nuc' in file:
+                f = open(top_dirs[id] + '/' + file)
+                for i, line in enumerate(f):
+                    if i == top_indices[id]:
+                        top_genes_nuc.append(line.replace('\n', '').split('\t'))
+                f.close()
+else:
+    for id in range(0, len(top_dirs)):
+        for file in os.listdir(top_dirs[id]):
+            if 'genes_nuc' in file:
+                f = open(top_dirs[id] + '/' + file)
+                for i, line in enumerate(f):
+                    if i == top_indices[id]:
+                        top_genes_nuc.append(line.replace('\n', '').split('\t'))
+                f.close()
+            if 'genes_mt' in file:
+                f = open(top_dirs[id] + '/' + file)
+                for i, line in enumerate(f):
+                    if i == top_indices[id]:
+                        top_genes_mt.append(line.replace('\n', '').split('\t'))
+                f.close()
+
+if experiment_type == 'mt':
+    for mt_genes_set in top_genes_mt:
+        config_file_name = 'config.txt'
+        with open(config_file_name, "w") as f:
+            f.write('experiment_type\t' + experiment_type + '\n')
+            f.write('reference_pop\t' + reference_pop + '\n')
+            f.write('target_pop\t' + target_pop + '\n')
+            f.write('reference_part\t' + str(reference_part) + '\n')
+            f.write('result_file_suffix\t' + result_file_suffix + '\n')
+            f.write('target_accuracy\t' + str(target_accuracy) + '\n')
+            f.write('num_features\t' + str(num_features) + '\n')
+            f.write('gene_files\t' + ', '.join(gene_files) + '\n')
+            f.write('mt_genes_set\t' + ', '.join(mt_genes_set) + '\n')
+        k_mt_max = len(mt_genes_set)
+        k_nuc_max = 1
+        for k_mt in range(k_mt_max, k_mt_max + 1):
+            for k_nuc in range(k_nuc_max, k_nuc_max + 1):
+                random_forest(k_mt, k_nuc)
+
+elif experiment_type == 'nuc':
+    for nuc_genes_set in top_genes_nuc:
+        config_file_name = 'config.txt'
+        with open(config_file_name, "w") as f:
+            f.write('experiment_type\t' + experiment_type + '\n')
+            f.write('reference_pop\t' + reference_pop + '\n')
+            f.write('target_pop\t' + target_pop + '\n')
+            f.write('reference_part\t' + str(reference_part) + '\n')
+            f.write('result_file_suffix\t' + result_file_suffix + '\n')
+            f.write('target_accuracy\t' + str(target_accuracy) + '\n')
+            f.write('num_features\t' + str(num_features) + '\n')
+            f.write('gene_files\t' + ', '.join(gene_files) + '\n')
+            f.write('nuc_genes_set\t' + ', '.join(nuc_genes_set) + '\n')
+        k_mt_max = 1
+        k_nuc_max = len(nuc_genes_set)
+        for k_mt in range(k_mt_max, k_mt_max + 1):
+            for k_nuc in range(k_nuc_max, k_nuc_max + 1):
+                random_forest(k_mt, k_nuc)
+
+else:
+    for gene_set_id in range(0, len(top_genes_mt)):
+        config_file_name = 'config.txt'
+        with open(config_file_name, "w") as f:
+            f.write('experiment_type\t' + experiment_type + '\n')
+            f.write('reference_pop\t' + reference_pop + '\n')
+            f.write('target_pop\t' + target_pop + '\n')
+            f.write('reference_part\t' + str(reference_part) + '\n')
+            f.write('result_file_suffix\t' + result_file_suffix + '\n')
+            f.write('target_accuracy\t' + str(target_accuracy) + '\n')
+            f.write('num_features\t' + str(num_features) + '\n')
+            f.write('gene_files\t' + ', '.join(gene_files) + '\n')
+            f.write('mt_genes_set\t' + ', '.join(top_genes_mt[gene_set_id]) + '\n')
+            f.write('nuc_genes_set\t' + ', '.join(top_genes_nuc[gene_set_id]) + '\n')
+        k_mt_max = len(top_genes_mt[gene_set_id])
+        k_nuc_max = len(top_genes_nuc[gene_set_id])
+        for k_mt in range(k_mt_max, k_mt_max + 1):
+            for k_nuc in range(k_nuc_max, k_nuc_max + 1):
+                random_forest(k_mt, k_nuc)
--- Source/mtDNA/cluster/rf.py
+++ Source/mtDNA/cluster/rf.py
@@ -27,7 +27,7 @@ def random_forest(k_mt, k_nuc):
     for line in f:
         line = line.replace('\n', '')
         items = line.split('\t')
-        if items[0] == 'gene_files':
+        if items[0] == 'gene_files' or items[0] == 'mt_genes_set' or items[0] == 'nuc_genes_set':
             config_dict[items[0]] = items[1].split(', ')
         else:
             config_dict[items[0]] = items[1]
@@ -40,10 +40,19 @@ def random_forest(k_mt, k_nuc):
 
     genes = []
     genes_ids = []
+    genes_set = []
+
+    if 'mt_genes_set' in config_dict:
+        genes_set.append([int(i) for i in config_dict['mt_genes_set']])
+    if 'nuc_genes_set' in config_dict:
+        genes_set.append([int(i) for i in config_dict['nuc_genes_set']])
 
     for file_id in range(0, len(config_dict['gene_files'])):
         data_gene_file = open(data_path + config_dict['gene_files'][file_id])
-        genes.append([line.replace('\n', '') for line in data_gene_file])
+        if len(genes_set) > 0:
+            genes.append([line.replace('\n', '') for i, line in enumerate(data_gene_file) if i in genes_set[file_id]])
+        else:
+            genes.append([line.replace('\n', '') for line in data_gene_file])
         genes_ids.append([])
         for gene_id in range(0, len(genes[file_id])):
             genes_ids[file_id].append(gene_id)
@@ -89,15 +98,20 @@ def random_forest(k_mt, k_nuc):
     results = Result()
     unit_task(unit_config, results)
 
-    experiment_result_path = result_path + \
+    experiment_result_path_with_k = result_path + \
                              unit_config.params_dict['experiment_type'] + '/' + \
                              'ref(' + unit_config.params_dict['reference_pop'] + ')_' + \
                              'target(' + unit_config.params_dict['target_pop'] + ')/' + \
                              'mt(' + str(unit_config.params_dict['k_mt']) + ')_' + \
                              'nuc(' + str(unit_config.params_dict['k_nuc']) + ')/'
 
+    experiment_result_path = result_path + \
+                             unit_config.params_dict['experiment_type'] + '/' + \
+                             'ref(' + unit_config.params_dict['reference_pop'] + ')_' + \
+                             'target(' + unit_config.params_dict['target_pop'] + ')/'
+
     try:
-        os.makedirs(experiment_result_path)
+        os.makedirs(experiment_result_path_with_k)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
@@ -107,26 +121,59 @@ def random_forest(k_mt, k_nuc):
         suffix += '_'
 
     if len(results.accuracy) > 0:
-        with open(experiment_result_path + suffix + 'accuracy.txt', 'w') as f:
-            for item in results.accuracy:
-                f.write("%s\n" % item)
+        if int(unit_config.params_dict['num_features']) == 0:
+            with open(experiment_result_path_with_k + suffix + 'accuracy.txt', 'w') as f:
+                for item in results.accuracy:
+                    f.write("%s\n" % item)
 
-        with open(experiment_result_path + suffix + 'num_features.txt', 'w') as f:
-            for item in results.num_features:
-                f.write("%s\n" % item)
+            with open(experiment_result_path_with_k + suffix + 'num_features.txt', 'w') as f:
+                for item in results.num_features:
+                    f.write("%s\n" % item)
 
-        if unit_config.params_dict['experiment_type'] == 'mt':
-            with open(experiment_result_path + suffix + 'genes_mt.txt', 'w') as f:
-                for item in results.mt_genes:
-                    f.write("%s\n" % '\t'.join(list(map(str, item))))
-        elif unit_config.params_dict['experiment_type'] == 'nuc':
-            with open(experiment_result_path + suffix + 'genes_nuc.txt', 'w') as f:
-                for item in results.nuc_genes:
-                    f.write("%s\n" % '\t'.join(list(map(str, item))))
+            if unit_config.params_dict['experiment_type'] == 'mt':
+                with open(experiment_result_path_with_k + suffix + 'genes_mt.txt', 'w') as f:
+                    for item in results.mt_genes:
+                        f.write("%s\n" % '\t'.join(list(map(str, item))))
+            elif unit_config.params_dict['experiment_type'] == 'nuc':
+                with open(experiment_result_path_with_k + suffix + 'genes_nuc.txt', 'w') as f:
+                    for item in results.nuc_genes:
+                        f.write("%s\n" % '\t'.join(list(map(str, item))))
+            else:
+                with open(experiment_result_path_with_k + suffix + 'genes_mt.txt', 'w') as f:
+                    for item in results.mt_genes:
+                        f.write("%s\n" % '\t'.join(list(map(str, item))))
+                with open(experiment_result_path_with_k + suffix + 'genes_nuc.txt', 'w') as f:
+                    for item in results.nuc_genes:
+                        f.write("%s\n" % '\t'.join(list(map(str, item))))
         else:
-            with open(experiment_result_path + suffix + 'genes_mt.txt', 'w') as f:
-                for item in results.mt_genes:
-                    f.write("%s\n" % '\t'.join(list(map(str, item))))
-            with open(experiment_result_path + suffix + 'genes_nuc.txt', 'w') as f:
-                for item in results.nuc_genes:
-                    f.write("%s\n" % '\t'.join(list(map(str, item))))
+            if unit_config.params_dict['experiment_type'] == 'mt':
+                with open(experiment_result_path + suffix +
+                          'top_features_mt(' + '_'.join(config_dict['mt_genes_set']) + ').txt', 'w') as f:
+                    features_count = 0
+                    for key, value in results.features.items():
+                        if value > 0.0 and features_count < int(unit_config.params_dict['num_features']):
+                            line = str(key) + '\t' + str(value)
+                            f.write(line)
+                            f.write('\n')
+                            features_count += 1
+            elif unit_config.params_dict['experiment_type'] == 'nuc':
+                with open(experiment_result_path + suffix +
+                          'top_features_nuc(' + '_'.join(config_dict['nuc_genes_set']) + ').txt', 'w') as f:
+                    features_count = 0
+                    for key, value in results.features.items():
+                        if value > 0.0 and features_count < int(unit_config.params_dict['num_features']):
+                            line = str(key) + '\t' + str(value)
+                            f.write(line)
+                            f.write('\n')
+                            features_count += 1
+            else:
+                with open(experiment_result_path + suffix +
+                          'top_features_mt(' + '_'.join(config_dict['mt_genes_set']) +
+                          ')_nuc(' + '_'.join(config_dict['nuc_genes_set']) + ').txt', 'w') as f:
+                    features_count = 0
+                    for key, value in results.features.items():
+                        if value > 0.0 and features_count < int(unit_config.params_dict['num_features']):
+                            line = str(key) + '\t' + str(value)
+                            f.write(line)
+                            f.write('\n')
+                            features_count += 1
