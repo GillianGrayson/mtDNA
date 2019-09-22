52318fe1b9a76da4526c98cd1fa7bef81891fff6
Unknown <aaron.blare@mail.ru>
2019-09-22 18:09:42 +0300

    Refactoring

--- Source/mtDNA/cluster/run.py
+++ Source/mtDNA/cluster/run.py
@@ -1,6 +1,8 @@
 from Source.mtDNA.cluster.rf import random_forest
 
-data_path = '../../../Data/'
+data_path = 'D:/Aaron/Bio/mtDNA/Data/'
+data_path_npz = 'D:/Aaron/Bio/mtDNA/Data/genes/npz/'
+data_path_pkl = 'D:/Aaron/Bio/mtDNA/Data/genes/pkl/'
 experiment_type = 'nuc'
 reference_pop = 'IBS'
 target_pop = 'FIN'
@@ -17,6 +19,9 @@ for k_mt in range(1, k_mt_max + 1):
     for k_nuc in range(1, k_nuc_max + 1):
         config_file_name = 'config.txt'
         with open(config_file_name, "w") as f:
+            f.write('data_path\t' + data_path + '\n')
+            f.write('data_path_npz\t' + data_path_npz + '\n')
+            f.write('data_path_pkl\t' + data_path_pkl + '\n')
             f.write('experiment_type\t' + experiment_type + '\n')
             f.write('reference_pop\t' + reference_pop + '\n')
             f.write('target_pop\t' + target_pop + '\n')
