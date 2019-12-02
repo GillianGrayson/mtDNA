17df4a9863f23a458e982ccdf14a69dc7fdcf684
AaronBlare <aaron.blare@mail.ru>
2019-12-02 13:04:25 +0300

    tibet added pickle fig saving

--- Source/mtDNA/tibet/functions/plot_functions.py
+++ Source/mtDNA/tibet/functions/plot_functions.py
@@ -1,5 +1,6 @@
 import plotly
 import plotly.graph_objs as go
+import pickle
 
 
 def plot_hist(data, suffix, file_path):
@@ -15,5 +16,17 @@ def plot_hist(data, suffix, file_path):
                       height=1000)
 
     plotly.offline.plot(fig, filename=file_path + suffix + '_hist.html', auto_open=False, show_link=True)
-    plotly.io.write_image(fig, file_path + suffix + '_hist.png')
-    plotly.io.write_image(fig, file_path + suffix + '_hist.pdf')
+    # plotly.io.write_image(fig, file_path + suffix + '_hist.png')
+    # plotly.io.write_image(fig, file_path + suffix + '_hist.pdf')
+
+    with open(file_path + suffix + '_hist_fig.pickle', 'wb') as handle:
+        pickle.dump(fig, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def plot_hist_from_pkl(suffix, file_path):
+    with open(file_path + suffix + '_hist_fig.pickle', 'rb') as handle:
+        fig_pkl = pickle.load(handle)
+
+    plotly.offline.plot(fig_pkl, filename=file_path + suffix + '_hist.html', auto_open=False, show_link=True)
+    plotly.io.write_image(fig_pkl, file_path + suffix + '_hist.png')
+    plotly.io.write_image(fig_pkl, file_path + suffix + '_hist.pdf')
